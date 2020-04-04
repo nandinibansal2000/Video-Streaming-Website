67636806afe00938741f368cdc2f4f36dab1cef7
drigil <49494567+drigil@users.noreply.github.com>
2020-04-04 20:21:15 +0530

    Add files via upload
    Functions for production house

---
+++ productionHouse.py
@@ -0,0 +1,26 @@
+#Anshul Mendiratta
+#Production House
+
+
+#SQL Queries
+
+def getMovies(ProductionHouse ProductionHouse):
+	#Get movies corresponding to production house
+
+def getMovieViewers(Movie movie):
+	#Get the list of people who watched the given movie
+
+#Python Queries
+
+def getMovieRating(Movie movie):
+	#Get rating according to movie
+
+def groupUsersByAge (UserList userList):
+	#get number of users belonging to a particular age
+
+def groupMoviesByGenre (MovieList movieList):
+	#get rating for each genre
+
+def getProductionHouseRating (MovieList movieList):
+	#get average rating of production house according to the list of movies provided
+
