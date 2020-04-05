3e6f6d0c1c0522dd9a7f62f2ee285e6ab1be4668
GoodwillHunter <46033050+GoodwillHunter@users.noreply.github.com>
2020-04-05 16:27:10 +0530

    Update productionHouse.py

--- productionHouse.py
+++ productionHouse.py
@@ -6,30 +6,39 @@
 
 def getMovies(ProductionHouse ProductionHouse):
 	#Get movies corresponding to production house
+	pass
 
 def getMovieViewers(Movie movie):
 	#Get the list of people who watched the given movie
+	pass
 	
 def getUpcomingMovie(ProductionHouse productionHouse):
 	#get upcoming movies corresponding to production house
+	pass
 
 def uploadMovie():
 	#upload movie
+	pass
 
 def removeMovie():
 	#remove movie
+	pass
 
 #Python Queries
 
 def getMovieRating(Movie movie):
 	#Get rating according to movie
+	pass
 
 def groupUsersByAge (UserList userList):
 	#get number of users belonging to a particular age
+	pass
 
 def groupMoviesByGenre (MovieList movieList):
 	#get rating for each genre
+	pass
 
 def getProductionHouseRating (MovieList movieList):
 	#get average rating of production house according to the list of movies provided
+	pass
 
