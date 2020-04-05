635feeaa715180cfa21ebabfad3d272d0631b940
sanskar27 <62891707+sanskar27@users.noreply.github.com>
2020-04-05 22:25:43 +0530

    Add files via upload

---
+++ Artist.py
@@ -0,0 +1,39 @@
+#Sanskar sachdeva
+#Artist
+
+#SQL Queries
+def get_specific_movie():
+    #this would return the info of a particuler movie
+    #like rating, no. of views etc.
+    pass
+
+def top_genre():
+    # this would return the genres with their respective ranks
+    # in a table.
+    pass
+
+def content():
+    # this would return all the movies, the artist has
+    # been  a part of available on the platform.
+    pass
+
+def artist_info():
+    # this returns info about the rating like his age,
+    # total movies acted in, movies he is best known for etc.
+    pass
+
+#Python Queries
+def artist_rating():
+    # this returns the avg rating of the artist given by the
+    # users, based on all his performances.
+    pass
+
+def artist_official_rating():
+    # this returns the official rating of the artist, 
+    # given by the rating authority like imdb.
+    pass
+
+def artist_rank():
+    # this would return the artist_rank among all others 
+    # in the form of a rank table based on their avg ratings.
+    pass
