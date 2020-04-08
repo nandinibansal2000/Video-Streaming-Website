961121ea4b9f2331ed5a137f4970b1c11b2e4178
drigil <49494567+drigil@users.noreply.github.com>
2020-04-09 00:05:10 +0530

    Functions added

--- productionHouse.py
+++ productionHouse.py
@@ -1,44 +1,137 @@
 #Anshul Mendiratta
 #Production House
 
+import mysql.connector
 
-#SQL Queries
+mydb = mysql.connector.connect(
 
-def getMovies(ProductionHouse ProductionHouse):
+	host="localhost",
+	user="Drigil",
+	passwd="Anshul12",
+	database = "dbmsproject",
+	auth_plugin="mysql_native_password"
+
+)
+
+if(mydb.is_connected()):
+	print("Successfully Connected")
+
+mycursor = mydb.cursor()
+
+
+# #SQL Queries
+def getMovies(productionHouse):
 	#Get movies corresponding to production house
-	pass
 
-def getMovieViewers(Movie movie):
+	productionHouseName = (productionHouse, )
+	sql_query = ("SELECT MOVIE_ID, GENRE FROM MOVIES WHERE P_HouseID IN (SELECT PHID FROM PRODUCTION_HOUSES WHERE NAME = (%s))")
+	mycursor.execute(sql_query, productionHouseName)
+	arr = mycursor.fetchall()
+	return arr
+
+def getMovieViewersFromMovie(movie):
 	#Get the list of people who watched the given movie
-	pass
-	
-def getUpcomingMovie(ProductionHouse productionHouse):
+
+	movieID = (movie, )
+	sql_query = ("SELECT UID FROM WATCH_LIST WHERE MOVIEID = %s")
+	mycursor.execute(sql_query, movieID)		
+	arr = mycursor.fetchall()
+	return arr
+
+def getMovieViewersFromMovieList(movieList):
+	#get all viewers of a publisher from their list of movies
+
+	viewerSet = set()
+	for i in movieList:
+		arr2 = getMovieViewersFromMovie(i[0])
+		for j in arr2:
+			viewerSet.add(j[0])
+	return viewerSet
+
+
+def getUpcomingMovies(productionHouse):
 	#get upcoming movies corresponding to production house
-	pass
 
-def uploadMovie():
+	productionHouseName = (productionHouse, )
+	sql_query = ("SELECT EMID FROM UPCOMING_MOVIES WHERE Production_HouseID IN (SELECT PHID FROM PRODUCTION_HOUSES WHERE NAME = (%s))")
+	mycursor.execute(sql_query, productionHouseName)
+	arr = mycursor.fetchall()
+	return arr
+
+def uploadMovie(productionHouseID, name, IMDB, duration, genre):
 	#upload movie
-	pass
 
-def removeMovie():
-	#remove movie
-	pass
+	sql_query = ("INSERT INTO MOVIES (P_HOUSEID, MOVIE_NAME, IMDB, DURATION, GENRE) VALUES (%s, %s, %s, %s, %s)")
+	entry = (productionHouseID, name, IMDB, duration, genre)
+	mycursor.execute(sql_query, entry)
+	#Uncomment when you want to make changes permanent
+	#mydb.commit()
 
-#Python Queries
+#Wont work right now due to foreign key constraint in Starcast
+def removeMovie(movie):
+	#remove movie from name
 
-def getMovieRating(Movie movie):
+	sql_query = ("DELETE FROM MOVIES WHERE MOVIE_NAME = %s")
+	movieName = (movie, )
+	mycursor.execute(sql_query, movieName)
+	# mycursor.execute("SELECT * FROM MOVIES")
+	# print(mycursor.fetchall())
+
+# #Python Queries
+
+def getMovieRatingFromID(movie):
 	#Get rating according to movie
-	pass
 
-def groupUsersByAge (UserList userList):
+	movieID = (movie, )
+	sql_query = ("SELECT AVG(RATING) FROM WATCH_LIST WHERE MOVIEID = %s")
+	mycursor.execute(sql_query, movieID)
+	avg = mycursor.fetchone()
+	avg = avg[0]
+	return avg
+
+
+def groupUsersByAge (userlist):
 	#get number of users belonging to a particular age
-	pass
 
-def groupMoviesByGenre (MovieList movieList):
+	userTuple = tuple(userlist)
+	sql_query = ("SELECT AGE, COUNT(UID) FROM USERS WHERE UID IN {} GROUP BY AGE").format(userTuple)
+	mycursor.execute(sql_query)
+	arr = mycursor.fetchall()
+	return arr
+
+def groupMoviesByGenre (movieList):
 	#get rating for each genre
-	pass
 
-def getProductionHouseRating (MovieList movieList):
+	ratingDict = {}
+	for i in movieList:
+		val = getMovieRatingFromID(i[0])
+		if(val==None):
+			val = 0
+		if(i[1] in ratingDict):
+			ratingDict[i[1]].append(val)
+		else:
+			ratingDict[i[1]] = [val]
+	for i in ratingDict.keys():
+		ratingDict[i] = sum(ratingDict[i])/len(ratingDict[i])
+	return ratingDict
+
+
+
+def getProductionHouseRating (movieList):
 	#get average rating of production house according to the list of movies provided
-	pass
 
+	ratingList = []
+	for i in movieList:
+		val = getMovieRatingFromID(i[0])
+		if(val==None):
+			val = 0
+		ratingList.append(val)
+	avg = sum(ratingList)/len(ratingList)
+	return avg
+
+######################################################################################	
+
+# Using functions - 
+# 1) First call getMovies to get movies corresponding to the production house
+# 2) use this value for functions wherever movielist is needed
+# 3) getMovieViewersFromMovieList gives list of users for that productionHouse, you can use this value for where userList is needed
