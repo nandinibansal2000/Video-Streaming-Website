5e61f1af0375bb202d0f1131605460ff1aedee44
drigil <49494567+drigil@users.noreply.github.com>
2020-04-10 00:59:51 +0530

    Graph functions added

--- productionHouse.py
+++ productionHouse.py
@@ -1,7 +1,11 @@
 #Anshul Mendiratta
 #Production House
 
+#graphs and mydb as argument
+
 import mysql.connector
+import pygal
+
 
 mydb = mysql.connector.connect(
 
@@ -16,11 +20,16 @@ mydb = mysql.connector.connect(
 if(mydb.is_connected()):
 	print("Successfully Connected")
 
-mycursor = mydb.cursor()
+
+def getCursor(mydb):
+	#get cursor from database
+	#Call this function first
+
+	return mydb.cursor()
 
 
 # #SQL Queries
-def getMovies(productionHouse):
+def getMovies(mycursor, productionHouse):
 	#Get movies corresponding to production house
 
 	productionHouseName = (productionHouse, )
@@ -29,7 +38,7 @@ def getMovies(productionHouse):
 	arr = mycursor.fetchall()
 	return arr
 
-def getMovieViewersFromMovie(movie):
+def getMovieViewersFromMovie(mycursor, movie):
 	#Get the list of people who watched the given movie
 
 	movieID = (movie, )
@@ -38,18 +47,18 @@ def getMovieViewersFromMovie(movie):
 	arr = mycursor.fetchall()
 	return arr
 
-def getMovieViewersFromMovieList(movieList):
+def getMovieViewersFromMovieList(mycursor, movieList):
 	#get all viewers of a publisher from their list of movies
 
 	viewerSet = set()
 	for i in movieList:
-		arr2 = getMovieViewersFromMovie(i[0])
+		arr2 = getMovieViewersFromMovie(mycursor, i[0])
 		for j in arr2:
 			viewerSet.add(j[0])
 	return viewerSet
 
 
-def getUpcomingMovies(productionHouse):
+def getUpcomingMovies(mycursor, productionHouse):
 	#get upcoming movies corresponding to production house
 
 	productionHouseName = (productionHouse, )
@@ -58,7 +67,7 @@ def getUpcomingMovies(productionHouse):
 	arr = mycursor.fetchall()
 	return arr
 
-def uploadMovie(productionHouseID, name, IMDB, duration, genre):
+def uploadMovie(mycursor, productionHouseID, name, IMDB, duration, genre):
 	#upload movie
 
 	sql_query = ("INSERT INTO MOVIES (P_HOUSEID, MOVIE_NAME, IMDB, DURATION, GENRE) VALUES (%s, %s, %s, %s, %s)")
@@ -68,7 +77,7 @@ def uploadMovie(productionHouseID, name, IMDB, duration, genre):
 	#mydb.commit()
 
 #Wont work right now due to foreign key constraint in Starcast
-def removeMovie(movie):
+def removeMovie(mycursor, movie):
 	#remove movie from name
 
 	sql_query = ("DELETE FROM MOVIES WHERE MOVIE_NAME = %s")
@@ -79,7 +88,7 @@ def removeMovie(movie):
 
 # #Python Queries
 
-def getMovieRatingFromID(movie):
+def getMovieRatingFromID(mycursor, movie):
 	#Get rating according to movie
 
 	movieID = (movie, )
@@ -90,7 +99,7 @@ def getMovieRatingFromID(movie):
 	return avg
 
 
-def groupUsersByAge (userlist):
+def groupUsersByAge (mycursor, userlist):
 	#get number of users belonging to a particular age
 
 	userTuple = tuple(userlist)
@@ -99,12 +108,12 @@ def groupUsersByAge (userlist):
 	arr = mycursor.fetchall()
 	return arr
 
-def groupMoviesByGenre (movieList):
+def groupMoviesByGenre (mycursor, movieList):
 	#get rating for each genre
 
 	ratingDict = {}
 	for i in movieList:
-		val = getMovieRatingFromID(i[0])
+		val = getMovieRatingFromID(mycursor, i[0])
 		if(val==None):
 			val = 0
 		if(i[1] in ratingDict):
@@ -117,7 +126,7 @@ def groupMoviesByGenre (movieList):
 
 
 
-def getProductionHouseRating (movieList):
+def getProductionHouseRating (mycursor, movieList):
 	#get average rating of production house according to the list of movies provided
 
 	ratingList = []
@@ -135,3 +144,30 @@ def getProductionHouseRating (movieList):
 # 1) First call getMovies to get movies corresponding to the production house
 # 2) use this value for functions wherever movielist is needed
 # 3) getMovieViewersFromMovieList gives list of users for that productionHouse, you can use this value for where userList is needed
+
+######################################################################################
+
+def graphGenreVSMovies(inpDict):
+	line_chart = pygal.HorizontalBar()
+	line_chart.title = "Ratings corresponding to each Genre"
+	for i in inpDict:
+		line_chart.add(i, inpDict[i])
+	line_chart.render_in_browser()
+
+def graphUsersVSAge(inpSet):
+	line_chart = pygal.HorizontalBar()
+	line_chart.title = "Number of users corresponding to Age"
+	for i in inpSet:
+		line_chart.add(str(i[0]), i[1])
+	line_chart.render_in_browser()
+
+######################################################################################
+
+# cursor = getCursor(mydb)
+# arr = getMovies(cursor, "Sony Pictures Motion Picture Group")
+# arr2 = groupMoviesByGenre(cursor, arr)
+# graphGenreVSMovies(arr2)
+
+# arr3 = getMovieViewersFromMovieList(cursor, arr)
+# arr4 = groupUsersByAge(cursor, arr3)
+# graphUsersVSAge(arr4)
