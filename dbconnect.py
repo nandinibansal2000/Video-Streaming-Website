2f0aa1e6e3e30e1831a9bec5131282beb2dfcd03
Nandini Bansal <nandini18056@iiitd.ac.in>
2020-04-24 18:53:52 +0530

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -1,13 +1,13 @@
-# import pymysql
-# pymysql.install_as_MySQLdb() 
-import MySQLdb
+# # import pymysql
+# # pymysql.install_as_MySQLdb() 
+# import MySQLdb
 
 
-def connection():
-	conn=MySQLdb.connect(host="localhost",
-						user="user",
-						passwd="password",
-						db="newdb")
-	c=conn.cursor()
-	return c,conn
+# def connection():
+# 	conn=MySQLdb.connect(host="localhost",
+# 						user="user",
+# 						passwd="password",
+# 						db="newdb")
+# 	c=conn.cursor()
+# 	return c,conn
 
