a4fef75f79287ba3dd496bf2588c8e8a6ad12faf
Nandini Bansal <nandini18056@iiitd.ac.in>
2020-04-24 18:52:13 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 # import pymysql
 # pymysql.install_as_MySQLdb()
-import MySQLdb
+# import MySQLdb
 from wtforms import Form, BooleanField , TextField, PasswordField, validators
 from flask import Flask,render_template,flash,request,url_for,redirect,session
 
 from passlib.hash import sha256_crypt
-from MySQLdb import escape_string as thwart
+# from MySQLdb import escape_string as thwart
 import gc
 import utkarsh
 import mysql.connector
